d00865888f52f9905b9d07401fbfbf22a19dc7db
Vincent <74841357+vhknip@users.noreply.github.com>
2022-09-08 21:45:16 -0700

    Update spectral_match.py
    Merged all match functions into single function with modes for direct, forward, and reverse matching. Fixed error with 0-intensity signals. MS devices generally don't output 0 values for spectral data, but with custom vectors, some of the match factor calculations were incorrect and were fixed in the updated function.

--- spectral_match.py
+++ spectral_match.py
@@ -57,10 +57,12 @@ def vector_map(vector: list, basis: set, target_basis: list) -> list:
     return mapped_vector
                 
     
+    
 def match(sample_sig: list,
           sample_mz: list,
           ref_sig: list,
           ref_mz: list,
+          mode: str = 'direct',
           mult: float = 100) -> float:
     """
     
@@ -75,6 +77,13 @@ def match(sample_sig: list,
         Signal values of each m/z value detected in reference compound as list
     ref_mz : list
         m/z values corresponding to each signal in reference compound as list
+    mode : {'direct', 'forward', 'reverse'}
+        modes for matching that include 'direct','forward', and 'reverse'
+        direct: normal match factor considering all values
+        forward: match factor considering only non-zero values found in sample
+            used for low concentrations / low ion abundance samples
+        reverse: match factor considering only non-zero values found in reference
+            used for high noise spectra
     mult: float
         Multiplier used for match factor score.
         Agilent (Unknowns Analysis and OpenLAB CDS) use 100 (default)
@@ -89,93 +98,29 @@ def match(sample_sig: list,
 
     """
     # construct common basis
-    common_basis = list(set(sample_mz + ref_mz))
+    common_basis = set(sample_mz + ref_mz)
     
+    # mape to desired basis
+    if mode == 'direct':
+        basis = list(set(sample_mz + ref_mz))
+    elif mode == 'forward':
+        basis = sample_mz
+        for signal in range(len(sample_sig)):
+            if sample_sig[signal] == 0:
+                ref_sig[signal] = 0
+    elif mode == 'reverse':
+        basis = ref_mz
+        for signal in range(len(ref_sig)):
+            if ref_sig[signal] == 0:
+                sample_sig[signal] = 0
+    else:
+        print('mode is defined as direct, forwaard, or reverse')
+
     # map both spectra to common basis
-    mapped_sample = vector_map(sample_sig, sample_mz, common_basis)
-    mapped_ref = vector_map(ref_sig, ref_mz, common_basis)
+    mapped_sample = vector_map(sample_sig, sample_mz, basis)
+    mapped_ref = vector_map(ref_sig, ref_mz, basis)
     
     # normalize basis
-    norm_sample = vector_norm(mapped_sample)
-    norm_ref = vector_norm(mapped_ref)
-    
-    return np.dot(norm_sample,norm_ref) * mult
-
-def fwd_match(sample_sig: list,
-          sample_mz: list,
-          ref_sig: list,
-          ref_mz: list,
-          mult: float = 100) -> float:
-    """
-    
-
-    Parameters
-    ----------
-    sample_sig : list
-        Signal values of each m/z value detected in sample compound as list
-    sample_mz : list
-        m/z values corresponding to each signal in sample compound as list
-    ref_sig : list
-        Signal values of each m/z value detected in reference compound as list
-    ref_mz : list
-        m/z values corresponding to each signal in reference compound as list
-    mult: float
-        Multiplier used for match factor score.
-        Agilent (Unknowns Analysis and OpenLAB CDS) use 100 (default)
-        Thermo Fisher (Chromeleon, Xcalibur, Tracefinder, NIST Lib) use 1000 
-
-    Returns
-    -------
-    match_factor : float
-        Quantification of similarity between both spectra
-        Essentially a dot product between two spectra
-            Need to ensure spectra are evalauted on correct vector space
-
-    """
-    
-    mapped_sample = vector_map(sample_sig, sample_mz, sample_mz)
-    mapped_ref = vector_map(ref_sig, ref_mz, sample_mz)
-    
-    norm_sample = vector_norm(mapped_sample)
-    norm_ref = vector_norm(mapped_ref)
-    
-    return np.dot(norm_sample,norm_ref) * mult
-
-def rev_match(sample_sig: list,
-          sample_mz: list,
-          ref_sig: list,
-          ref_mz: list,
-          mult: float = 100) -> float:
-    """
-    
-
-    Parameters
-    ----------
-    sample_sig : list
-        Signal values of each m/z value detected in sample compound as list
-    sample_mz : list
-        m/z values corresponding to each signal in sample compound as list
-    ref_sig : list
-        Signal values of each m/z value detected in reference compound as list
-    ref_mz : list
-        m/z values corresponding to each signal in reference compound as list
-    mult: float
-        Multiplier used for match factor score.
-        Agilent (Unknowns Analysis and OpenLAB CDS) use 100 (default)
-        Thermo Fisher (Chromeleon, Xcalibur, Tracefinder, NIST Lib) use 1000 
-
-    Returns
-    -------
-    match_factor : float
-        Quantification of similarity between both spectra
-        Essentially a dot product between two spectra
-            Need to ensure spectra are evalauted on correct vector space
-
-    """
-    
-    mapped_sample = vector_map(sample_sig, sample_mz, ref_mz)
-    mapped_ref = vector_map(ref_sig, ref_mz, ref_mz)
-    
     norm_sample = vector_norm(mapped_sample)
     norm_ref = vector_norm(mapped_ref)
     
